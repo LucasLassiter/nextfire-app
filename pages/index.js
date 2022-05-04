fb28b8380d4747a9fd1590e312c6471f6e8c892b
LucasLassiter <lucaslassiter@yahoo.com>
2022-05-03 22:00:45 -0700

    added things

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+import Loader from '../components/Loader';
+import toast from 'react-hot-toast';
 
 export default function Home() {
   return (
     <div>
       <h1>Hello world!</h1>
+      <Loader show />
+      <button onClick={() => toast.success('hello toast!')}>
+        Click for Toast!
+      </button>
     </div>
   )
 }
